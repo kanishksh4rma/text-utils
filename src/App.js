8c20ab3031adb33889030b13cc24678e912ee059
Kanishk Sharma <61106875+kanishksh4rma@users.noreply.github.com>
2021-09-03 20:21:38 +0530

    Deploy without router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import NavBar from './components/NavBar';
 import Form from './components/Form'
 import Alert from './components/Alert';
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  withRouter,
 
-} from "react-router-dom";
 import About from './components/About';
 
 function App() {
@@ -24,24 +17,12 @@ function App() {
   }
   return (
     <>
-      <Router>
-        <NavBar />
-        <Alert txt={alert} />
-
-{/* React Router */}
-        <Switch>
-          <Route exact path="/">
-            <div className="container my-3">
-              <h1>Enter a text here : </h1>
-              <Form showAlert={showAlert} />
-            </div>
-          </Route>
-            <Route exact path="/about" component={withRouter(About)} />
-
-          
-        </Switch>
-      </Router>
-
+      <NavBar />
+      <Alert txt={alert} />
+      <div className="container my-3">
+        <h1>Enter a text here : </h1>
+        <Form showAlert={showAlert} />
+      </div>
     </>
   );
 }
